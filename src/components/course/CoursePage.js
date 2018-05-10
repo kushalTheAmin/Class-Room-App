b9dd1728e7f5f8ee62ce4a8ae6694d571d38af2e
Kushal Amin <kushalamin0@gmail.com>
2018-05-10 02:12:47 -0400

    add course page

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -2,10 +2,47 @@ import React, { Component, PropTypes} from 'react';
 
 
 class CoursePage extends Component {
+
+  constructor(props,context)
+  {
+    super (props,context);
+
+    this.state =
+      {
+        course : {title : null}
+      };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickAdd = this.onClickAdd.bind(this);
+  }
+
+  onTitleChange(event)
+  {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState ({ course });
+  }
+
+  onClickAdd()
+  {
+
+  }
+
+
   render(){
     return(
       <div>
-        courses @@@
+        Course :
+        <div>
+          <input type="text"
+                 value = {this.state.course.title}
+                 onChange= {this.onTitleChange}
+          />
+
+          <input type = "submit"
+                 value ="ADD"
+                 onClick = {this.onClickAdd}
+          />
+        </div>
       </div>
     );
   }
